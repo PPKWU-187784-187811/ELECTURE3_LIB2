50b5b2653d549706e5ccbb19e26187c089b9909a
msut <marr994@o2.pl>
2016-11-27 17:49:23 +0100

    add exception throw

--- src/main/java/interfaces/ChecksumCalculator.java
+++ src/main/java/interfaces/ChecksumCalculator.java
@@ -1,9 +1,12 @@
 package interfaces;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by mariusz on 26.11.16.
  */
 public interface ChecksumCalculator {
 
-    String calculate(String fileName, String algorithm);
+    String calculate(String fileName, String algorithm) throws NoSuchAlgorithmException, IOException;
 }
